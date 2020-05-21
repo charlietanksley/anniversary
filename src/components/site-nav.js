ad06fa0cc2b968848866dcfc262d56a9f354b4ff
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-05-21 19:32:39 -0400

    reorder the links in the nav

--- src/components/site-nav.js
+++ src/components/site-nav.js
@@ -8,13 +8,13 @@ export default () => (
         {" "}
         <li>Home</li>
       </Link>
-      <Link to="/guestbook">
-        {" "}
-        <li>Read Guestbook</li>
-      </Link>
       <Link to="/photo-album">
         {" "}
         <li>Photo Album</li>
+      </Link>
+      <Link to="/guestbook">
+        {" "}
+        <li>Read Guestbook</li>
       </Link>
       <Link to="/guestbook#add-message">
         {" "}
