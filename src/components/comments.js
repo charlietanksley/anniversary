fb02a8128fc4c449aaf2118275ce030718bd3c31
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-05-21 07:05:03 -0400

    wrap line breaks in messages as formatted

--- src/components/comments.js
+++ src/components/comments.js
@@ -18,8 +18,8 @@ export default () => (
     render={data =>
       data.allContactCsv.edges.map((row, i) => (
         <div key={i} className="message-display">
-          <p className="message-body"> {row.node.message}</p>
-          <p className="message-sender"> {row.node.name}</p>
+          <p className="message-body">{row.node.message}</p>
+          <p className="message-sender">{row.node.name}</p>
         </div>
       ))
     }
