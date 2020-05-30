ce55c8e5076a05b73fa5e36e8c09c0c5991aa959
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-05-30 15:21:57 -0400

    brings in the 70s

--- src/components/image.js
+++ src/components/image.js
@@ -16,6 +16,135 @@ import Img from "gatsby-image"
 const Image = props => {
   const data = useStaticQuery(graphql`
     query {
+      cover: file(relativePath: { eq: "COVER.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      babyJoy70: file(relativePath: { eq: "1970/70-baby-joy.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      dogAndBaby70: file(relativePath: { eq: "1970/70-dog-and-baby.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      familyByFireplace70: file(
+        relativePath: { eq: "1970/70-family-by-fireplace.JPG" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      juneInFullDress70: file(
+        relativePath: { eq: "1970/70-june-in-full-dress.JPG" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      pinning70: file(relativePath: { eq: "1970/70-pinning.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      ringWeekendOffice70: file(
+        relativePath: { eq: "1970/70-ring-weekend-office.JPG" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      ringWeekend70: file(relativePath: { eq: "1970/70-ring-weekend.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      saberArch70: file(relativePath: { eq: "1970/70-saber-arch.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      sightseeing70: file(relativePath: { eq: "1970/70-sightseeing.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      vampires70: file(relativePath: { eq: "1970/70-vampires.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      wedding70: file(relativePath: { eq: "1970/70-wedding.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      whiteTie70: file(relativePath: { eq: "1970/70-white-tie.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      withIngrums70: file(relativePath: { eq: "1970/70-with-ingrums.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      withTrammels70: file(relativePath: { eq: "1970/70-with-trammels.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      yellowDress70: file(relativePath: { eq: "1970/70-yellow-dress.JPG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
       alaska: file(relativePath: { eq: "alaska.jpg" }) {
         childImageSharp {
           fluid {
--- src/components/seventies-and-eighties.js
+++ src/components/seventies-and-eighties.js
@@ -1,16 +1,20 @@
 import Decade from "../components/decade"
+import FullWidth from "../components/full-width"
+import TwoColumn from "../components/two-column"
 import Image from "../components/image"
 import React from "react"
 
 export default () => (
   <Decade headerName="1970s and 1980s">
-    <div className="two-column">
-      <div className="left-2-column">
-        <Image id="wedding" />
-      </div>
-      <div className="right-2-column">
-        <Image id="june1970" />
-      </div>
-    </div>
+    <TwoColumn right="pinning70" left="ringWeekendOffice70" />
+    <TwoColumn right="ringWeekend70" left="juneInFullDress70" />
+    <TwoColumn right="yellowDress70" left="whiteTie70" />
+    <TwoColumn right="wedding70" left="saberArch70" />
+    <TwoColumn right="withIngrums70" left="withTrammels70" />
+    <FullWidth img="sightseeing70" />
+    <FullWidth img="dogAndBaby70" />
+    <FullWidth img="babyJoy70" />
+    <FullWidth img="familyByFireplace70" />
+    <TwoColumn right="vampires70" left="vampires70" />
   </Decade>
 )
