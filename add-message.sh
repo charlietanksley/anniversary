fc6c936ef2594f3dd6f9d0f6b059269f2df991dc
Charlie Tanksley <charlie.tanksley@gmail.com>
2020-05-31 18:27:53 -0400

    fixes linebreaks in messages

--- add-message.sh
+++ add-message.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-cat "$1" | sed 's/","/"|"/g' | cut -d'|' -f1,2 | sed 's/"|"/","/g' > src/data/contact.csv
+
+cat "$1" | sed 's/","/"|"/g' | cut -d'|' -f1,2 | sed 's/"|"/","/g' | sed 's/\\r\\n/<>/g' | sed -r "s/(<>)+/<>/g" | tr "<>" "\n" > src/data/contact.csv
